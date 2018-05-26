7bf681fd114b55c484af59747f01a2eb646c528f
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2018-05-26 15:26:38 -0400

    Atualizado efeito do conteúdo da página inicial.

--- js/default.js
+++ js/default.js
@@ -6,55 +6,42 @@
     // Scroll Reveal Options
     var options = {
         // 'bottom', 'left', 'top', 'right'
-        origin: 'top',
-
+        origin: 'bottom',
         // Can be any valid CSS distance, e.g. '5rem', '10%', '20vw', etc.
         distance: '20px',
-
         // Time in milliseconds.
         duration: 0,
         delay: 0,
-
         // Starting angles in degrees, will transition from these values to 0 in all axes.
         rotate: {x: 0, y: 0, z: 0},
-
         // Starting opacity value, before transitioning to the computed opacity.
-        opacity: 0,
-
+        opacity: 1,
         // Starting scale value, will transition from this value to 1
         scale: 1,
-
         // Accepts any valid CSS easing, e.g. 'ease', 'ease-in-out', 'linear', etc.
         // easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
         easing: 'ease',
-
         // `<html>` is the default reveal container. You can pass either:
         // DOM Node, e.g. document.querySelector('.fooContainer')
         // Selector, e.g. '.fooContainer'
         container: window.document.documentElement,
-
         // true/false to control reveal animations on mobile.
         mobile: true,
-
         // true:  reveals occur every time elements become visible
         // false: reveals occur once as elements become visible
         reset: true,
-
         // 'always' — delay for all reveal animations
         // 'once'   — delay only the first time reveals occur
         // 'onload' - delay only for animations triggered by first load
-        useDelay: 'always',
-
+        useDelay: 'once',
         // Change when an element is considered in the viewport. The default value
         // of 0.20 means 20% of an element must be visible for its reveal to occur.
         viewFactor: 0.30,
-
         // Pixel values that alter the container boundaries.
         // e.g. Set `{ top: 48 }`, if you have a 48px tall fixed toolbar.
         // --
         // Visual Aid: https://scrollrevealjs.org/assets/viewoffset.png
         viewOffset: {top: 0, right: 0, bottom: 0, left: 0},
-
         // Callbacks that fire for each triggered element reveal, and reset.
         // Importante! A classe para iniciar as animações devem ser aplicadas antes do início
         // das animações da biblioteca, do contrário pode ter um efeito indesejado.
@@ -64,13 +51,11 @@
         beforeReset: function (domEl) {
             j(domEl).addClass('off').removeClass('on');
         },
-
         // Callbacks that fire for each completed element reveal, and reset.
         // Importante! A classe para iniciar as animações devem ser aplicadas antes do início
         // das animações da biblioteca, do contrário pode ter um efeito indesejado.
         afterReveal: function (domEl) {
             // j(domEl).addClass('on').removeClass('off');
-
         },
         afterReset: function (domEl) {
             // j(domEl).addClass('off').removeClass('on');
@@ -86,5 +71,4 @@
         window.sr.reveal('.three');
         window.sr.reveal('.four');
     });
-
 }(this, this.$ || this.jQuery || false));
